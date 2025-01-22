5e6ac91b14e084760d3897a72f71e048081d97d1
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-01-22 22:55:33 +0530

    added the product dettails component to the wishlist + related changes

--- client/src/components/shopping-view/wishlist.jsx
+++ client/src/components/shopping-view/wishlist.jsx
@@ -1,20 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Card } from "../ui/card"
 import WishlistProductTile from "./wishlist-product-tiile"
+import { fetchProductDetails } from "@/store/shop/products-slice";
+import ProductDetailsDialog from "./product-details";
+import { useEffect, useState } from "react";
 
 function Wishlist({ wishlistItems }) {
 
+    const { productDetails } = useSelector(state => state.shopProducts)
+    const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+
+    const dispatch = useDispatch()
+
+    function handleGetProductDetails(getCurrentProductId) {
+        dispatch(fetchProductDetails(getCurrentProductId));
+    }
+
+    useEffect(() => {
+        if (productDetails !== null) setOpenDetailsDialog(true)
+    }, [productDetails])
     return (
-        <Card className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4 md:p-6">
-            {
-                wishlistItems && wishlistItems.length > 0 ?
-                    wishlistItems.map(item =>
-                        <WishlistProductTile
-                            wishlistItem={item}
-                            key={item.productId}
-                        />) :
-                    null
-            }
-        </Card>
+        <div>
+            <Card className="grid grid-cols-1 md:grid-cols-2 gap-2 p-4 md:p-6">
+                {
+                    wishlistItems && wishlistItems.length > 0 ?
+                        wishlistItems.map(item =>
+                            <WishlistProductTile
+                                handleGetProductDetails={handleGetProductDetails}
+                                wishlistItem={item}
+                                key={item.productId}
+                            />) :
+                        null
+                }
+            </Card>
+
+            <ProductDetailsDialog
+                open={openDetailsDialog} setOpen={setOpenDetailsDialog}
+                productDetails={productDetails}
+            />
+        </div>
+
     );
 }
 
