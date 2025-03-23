45bed1b98d17b9fcfd438fa00d3ec62ab433f29d
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-03-23 05:44:18 +0530

    updated styling issues in order components

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
-import { Dialog } from "../ui/dialog";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
-import ShoppingOrderDetailsView from "./order-details";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "../ui/table";
+// import { ShoppingOrderTile } from "./order-tile"
+// import { ShoppingOrderDetailsDialog } from './order-details'
 import { useDispatch, useSelector } from "react-redux";
-import { getAllOrdersByUserId, getOrderDetails, resetOrderDetails } from "@/store/shop/order-slice";
-import { Badge } from "../ui/badge";
-
+import { getAllOrdersByUserId, getOrderDetails } from "@/store/shop/order-slice";
+import ShoppingOrderDetailsDialog from "./order-details";
+import ShoppingOrderTile from "./order-tile";
 
 
 
@@ -18,8 +17,8 @@ function ShoppingOrders() {
     const { user } = useSelector(state => state.auth)
     const { orderList, orderDetails } = useSelector((state) => state.shopOrder);
 
-    function handleFetchOrderDetails(getId) {
-        dispatch(getOrderDetails(getId));
+    function handleGetOrderDetails(getCurrentOrderId) {
+        dispatch(getOrderDetails(getCurrentOrderId));
     }
 
     useEffect(() => {
@@ -34,65 +33,44 @@ function ShoppingOrders() {
     // console.log(orderDetails, "orderDetails");
 
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Order History</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Order ID</TableHead>
-                            <TableHead>Order Date</TableHead>
-                            <TableHead>Order Status</TableHead>
-                            <TableHead>Order Price</TableHead>
-                            <TableHead>
-                                <span className="sr-only">Details</span>
-                            </TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {orderList && orderList.length > 0 ?
-                            orderList.map((orderItem) => (
-                                <TableRow key={orderItem?._id}>
-                                    <TableCell>{orderItem?._id}</TableCell>
-                                    <TableCell>{orderItem?.orderDate.split("T")[0]}</TableCell>
-                                    <TableCell>
-                                        <Badge
-                                            className={`py-1 px-3 ${orderItem?.orderStatus === "confirmed"
-                                                ? "bg-green-500"
-                                                : orderItem?.orderStatus === "rejected"
-                                                    ? "bg-red-600"
-                                                    : "bg-black"
-                                                }`}
-                                        >
-                                            {orderItem?.orderStatus}
-                                        </Badge>
-                                    </TableCell>
-                                    <TableCell>${orderItem?.totalAmount}</TableCell>
-                                    <TableCell>
-                                        <Dialog
-                                            open={openDetailsDialog}
-                                            onOpenChange={() => {
-                                                setOpenDetailsDialog(false);
-                                                dispatch(resetOrderDetails())
-                                            }}
-                                            className="max-h-[400px] overflow-hidden my-1"
-                                        >
-                                            <Button
-                                                onClick={() => handleFetchOrderDetails(orderItem?._id)}>
-                                                View Details
-                                            </Button>
-                                            <ShoppingOrderDetailsView orderDetails={orderDetails} />
-                                        </Dialog>
-                                    </TableCell>
-                                </TableRow>
-                            )) :
-                            null}
-                    </TableBody>
-                </Table>
-            </CardContent>
-        </Card>
+        <div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Order History</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Order ID</TableHead>
+                                <TableHead>Order Date</TableHead>
+                                <TableHead>Order Status</TableHead>
+                                <TableHead>Order Price</TableHead>
+                                <TableHead>
+                                    <span className="sr-only">Details</span>
+                                </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {orderList && orderList.length > 0 ?
+                                orderList.map((orderItem) => (
+                                    <ShoppingOrderTile
+                                        key={orderItem?._id}
+                                        orderItem={orderItem}
+                                        handleGetOrderDetails={handleGetOrderDetails}
+                                    />
+                                )) :
+                                null}
+                        </TableBody>
+                    </Table>
+                </CardContent>
+            </Card>
+
+            <ShoppingOrderDetailsDialog
+                open={openDetailsDialog} setOpen={setOpenDetailsDialog}
+                orderDetails={orderDetails}
+            />
+        </div>
     );
 }
 
